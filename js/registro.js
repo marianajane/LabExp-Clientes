79416674e7aac337b728b7e851795beea015147d
dianix <dianavazquez.dcg@gmail.com>
2017-07-07 12:09:52 -0500

    validacion austera para login

--- js/registro.js
+++ js/registro.js
@@ -1,8 +1,19 @@
 var cargarPagina = function () {
-    $('#login-form').validator()
-    $('#registro-form').validator()
-    
+    $('#login-form').submit(validarAcceso);    
 };
+
+var validarAcceso = function(e){
+    e.preventDefault();
+    var usuario = $("#user-email");
+    var contrasena = $("#user-password");
+    console.log(usuario.val(),contrasena.val())
+    
+    if (usuario.val() === "patitoadm@gmail.com" && contrasena.val() === "contra1X") {
+        location.href = "files/clients-search.html";
+    } else {
+        alert("intenta de nuevo")
+    }
+}
 
 
 $(document).ready(cargarPagina);
